52295fe1df026339858e1a47111be4cf67ed8f26
Gianluca Della Vedova <gianluca@dellavedova.org>
2016-03-16 10:55:44 +0100

    Make deploy runnable in a cron job
    Whenever pinw is updated, automatically create and run the new container.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-cd ../Docker/
+cd ~/pinw-deploy/Docker/
+v=`date +%F@%T`
 
 docker build -t pinw .
-docker run -d -P  --name "pinw1" pinw
+for cont in `docker ps -f name=pinw -q`
+do
+    docker rm -f $cont
+done
+docker run -d -P -e $PINW_NAME  --name "pinw.${v}" pinw
