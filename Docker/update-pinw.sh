31317280093bf2d8b80838a84b426d3c7f9492b5
Gianluca Della Vedova <gianluca@dellavedova.org>
2016-03-16 17:49:58 +0100

    Cleanup

--- Docker/update-pinw.sh
+++ Docker/update-pinw.sh
@@ -2,3 +2,5 @@
 
 cd /home/app
 sudo -u app git pull --all
+apt-get update
+apt-get upgrade -y -o Dpkg::Options::="--force-confold"
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-cd ~/pinw-deploy/Docker/
+#cd ~/pinw-deploy/Docker/
 v=`date +%FH%H%M`
 
-docker build -t pinw .
+docker build -t algolab/pinw:${v} .
 for cont in `docker ps -f name=pinw -q`
 do
     docker rm -f $cont
 done
 echo "Starting container pinw.${v} as $PINW_NAME"
-docker run -d -e VIRTUAL_HOST=$PINW_NAME -v /home/pinw/pinw-data:/data --name "pinw.${v}" pinw
+docker run -d -e VIRTUAL_HOST=$PINW_NAME -v /home/pinw/pinw-data:/data --name "pinw" algolab/pinw:${v}
+
+# Cleanup unused containers and images
+docker rm $(docker ps -a -q)
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
